070d695a79f6560e888a7d3e0fd7c0f0fa4fc1f4
unknown <olysiuk01@gmail.com>
2020-11-09 12:59:14 +0200

    added animate css library

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,23 @@
-window.onload = function(){
+new WOW().init()
+
+window.onload = function () {
   document.body.style.overflow = 'initial'
-  document.querySelector('.preloader').style.display = 'none';
+  document.querySelector('.preloader').style.display = 'none'
 }
 
+function menuBurger() {
+  let burger = document.querySelector('.header__menu-burger')
+  let menu = document.querySelector('.header__list')
 
-function menuBurger(){
-  let burger = document.querySelector('.header__menu-burger');
-  let menu = document.querySelector('.header__list');
-
-  burger.onclick = function(){
-    burger.classList.toggle('header__menu-burger--active');
-    menu.classList.toggle('header__list--open');
+  burger.onclick = function () {
+    burger.classList.toggle('header__menu-burger--active')
+    menu.classList.toggle('header__list--open')
     document.body.classList.toggle('hidden')
   }
 }
 
 menuBurger()
 
-
-document.querySelector('.footer__top-form').onsubmit = function(e){
+document.querySelector('.footer__top-form').onsubmit = function (e) {
   e.preventDefault()
 }
